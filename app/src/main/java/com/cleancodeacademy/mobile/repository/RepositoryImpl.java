3d9fcbd49f4c353dbb60ba5407975821f4914d88
vikas patel <vikas.patel@mastercard.com>
2018-06-08 19:12:02 +0530

    IRepository & Impl commit

---
+++ app/src/main/java/com/cleancodeacademy/mobile/repository/RepositoryImpl.java
@@ -0,0 +1,15 @@
+package com.cleancodeacademy.mobile.repository;
+
+import com.cleancodeacademy.domain.Repository.IRepository;
+import com.cleancodeacademy.domain.models.Author;
+
+import java.util.List;
+
+public class RepositoryImpl implements IRepository {
+
+
+    @Override
+    public void storeAuthors(List<Author> authors) {
+
+    }
+}
--- domain/src/main/java/com/cleancodeacademy/domain/Repository/IRepository.java
+++ domain/src/main/java/com/cleancodeacademy/domain/Repository/IRepository.java
@@ -1,4 +1,10 @@
 package com.cleancodeacademy.domain.Repository;
 
+import com.cleancodeacademy.domain.models.Author;
+
+import java.util.List;
+
 public interface IRepository {
+
+    public void storeAuthors(List<Author> authors);
 }
