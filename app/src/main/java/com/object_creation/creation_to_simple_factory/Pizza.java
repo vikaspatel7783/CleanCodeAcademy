692adfcb4923ff9c1447533cb013610bb6981ffa
vikas patel <vikas.patel@mastercard.com>
2018-08-27 19:11:50 +0530

    creation method - moving creation to simple factory

--- app/src/main/java/com/object_creation/creation_to_simple_factory/Pizza.java
+++ app/src/main/java/com/object_creation/creation_to_simple_factory/Pizza.java
@@ -12,4 +12,33 @@ class Pizza {
 
     Pizza(String corn, String blackOlives, String onion, String capsicum, String mushrooms, String tomatoes, String jalapenos) {}
 
+    /*public static Pizza createMargheritaWithSingleCheesePizza() {
+        return new Pizza();
+    }
+
+    public static Pizza createMargheritaWithDoubleCheesePizza() {
+        return new Pizza("DoubleCheese");
+    }
+
+    public static Pizza createFarmHousePizza() {
+        return new Pizza("veggies", "capsicum", "mushroom", "tomatoes");
+    }
+
+    public static Pizza createMaxicanGreenPizza() {
+        return new Pizza("onion", "capsicum", "tomatoes", "jalapenos", "herbs");
+    }
+
+    public static Pizza createExtraVagenzaPizza() {
+        return new Pizza("corn", "blackOlives", "onion", "capsicum", "mushrooms", "tomatoes", "jalapenos");
+    }*/
+
+    public void printAboutUs() {}
+
+    public void locateTheStore() {}
+
+    public void shareFeedback(String feedback) {}
+
+    public void rateUs(String ratings) {}
+
+    public void fetchFoodDeliveryParterners() {}
 }
